2c20953839b752063a5b6ae887e00ab52813f0b3
FenixVu442 <104748038+FenixVu442@users.noreply.github.com>
2022-07-25 23:35:12 -0500

    Lab 5 Completed

---
+++ Lab5/Debug/motion.d
@@ -0,0 +1,54 @@
+# FIXED
+
+motion.obj: ../motion.c
+motion.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+motion.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+motion.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+motion.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+motion.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+motion.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+motion.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+motion.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+motion.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+motion.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+motion.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+motion.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+motion.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+motion.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+motion.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+motion.obj: ../motion.h
+
+../motion.c:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h:
+
+../motion.h:
+
